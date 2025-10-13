3dd4f01b370bcd824dd0887f36403c519d96bfae
GitLab CI <ci@example.com>
2025-10-13 16:51:30 +0200

    Fix observer

--- src/Observers/CreateUUIDForWebhookRegistration.php
+++ src/Observers/CreateUUIDForWebhookRegistration.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace McGo\BoundLessWebhooks\Observers;
+namespace McGo\Webhooks\Observers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
-use McGo\BoundLessWebhooks\Models\WebhookRegistration;
+use McGo\Webhooks\Models\WebhookRegistration;
 
 class CreateUUIDForWebhookRegistration
 {
@@ -13,6 +13,5 @@ class CreateUUIDForWebhookRegistration
         $webhookRegistration->uuid = Str::uuid();
         $webhookRegistration->registered_by_ip = request()->ip();
         $webhookRegistration->registered_by_user_agent = request()->userAgent();
-        $webhookRegistration->user_id = Auth::id();
     }
 }
